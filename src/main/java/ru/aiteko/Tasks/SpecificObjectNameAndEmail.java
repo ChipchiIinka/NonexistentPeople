ff26a125b060f6d7e03acfd71929fc4b0789b546
ChipchiIinka <m.gi2016@yandex.ru>
2023-11-29 15:54:07 +0300

    SpecificObject fix

--- src/main/java/ru/aiteko/Tasks/SpecificObjectNameAndEmail.java
+++ src/main/java/ru/aiteko/Tasks/SpecificObjectNameAndEmail.java
@@ -11,11 +11,13 @@ public class SpecificObjectNameAndEmail implements Runnable{
         Root root = GsonParser.parse();
         assert root != null;
 
+        System.out.println("Особые пользователи с характеристиками: Активный, старше 30 лет, зарплата меньше 100" +
+                " и есть почта example.com");
         root.data.stream()
                 .filter(object -> CalculatingAge.CalculatingAgeFromBirthday(object.getBirthday()) > 30 &&
                         object.getStatus().equals(Status.ACTIVE) &&
                         object.getEmails().stream().anyMatch(email -> email.endsWith("example.com")) &&
-                        object.getMonthlySalary().compareTo(BigDecimal.valueOf(300)) < 0)
+                        object.getMonthlySalary().compareTo(BigDecimal.valueOf(100)) < 0)
                 .forEach(object -> System.out.println(object.name + " emails: " + object.getEmails()));
         System.out.println();
     }
